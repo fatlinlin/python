4d118799d2e882a0c0a51bbe66a2076e24ac9fb7
U-FRONTLAN\sberg <sberg@EFRONT-0-00189.frontlabs.com>
2012-09-17 10:39:53 +0200

    created lib file and tool file

--- merge.py
+++ merge.py
@@ -1,95 +1,14 @@
-import pysvn
-import os
+import argparse
+import tree
 
-DEFAULT_SVN_ROOT = r"https://src.frontsrv.com/svn/repository/branches4/rsk"
-DEFAULT_DISK_ROOT = r"c:\svn"
-
-class Branch(object):
-
-    def __init__(self, name):
-        self.name = name
-        self.disk_root = DEFAULT_DISK_ROOT
-        self.svn_root = DEFAULT_SVN_ROOT
-
-    @property
-    def svn_path(self):
-        return "{}/{}".format(self.svn_root, self.name)
-
-    @property
-    def disk_path(self):
-        return os.path.join(self.disk_root, self.name)
-
-    def next(self):
-        raise NotImplementedError()
-
-    def __repr__(self):
-        return "{}(name={})".format(self.__class__.__name__, self.name)
-
-class ClientBranch(Branch):
-
-    def __init__(self, name, parent):
-        super(ClientBranch, self).__init__(name)
-        self.parent = parent
-
-    def next(self):
-        return self.parent.next_version
-
-class MainBranch(Branch):
-
-    def __init__(self, name, next_version):
-        super(MainBranch, self).__init__(name)
-        self.next_version = next_version
-
-    def next(self):
-        return self.next_version
-
-class Trunk(Branch):
-
-    def __init__(self):
-        super(Trunk, self).__init__("trunk4.1")
-        self.svn_root = r"https://src.frontsrv.com/svn/repository"
-
-    def next(self):
-        raise StopIteration()
-        
-class Tree:
-
-    def __init__(self):
-        self.branches = {"trunk4.1" : Trunk()}
-        self.svn = pysvn.Client()
-
-    def load_main_branches(self, names, head):
-        try:
-            name = names.pop()
-        except IndexError:
-            return []
-        branch = MainBranch(name, head)
-        self.branches[name] = branch
-        return self.load_main_branches(names, branch) + [branch]
-
-    def load_client_branches(self, client_grps, main_branches):
-        for main, clients in zip(main_branches, client_grps):
-            for client in clients:
-                self.branches[client] = ClientBranch(client, main)
-   
-    def load_tree(self, branches):
-        main_branches_names, clients_grps = zip(*branches)
-        main_branches = self.load_main_branches(list(main_branches_names), self.branches["trunk4.1"])
-        self.load_client_branches(clients_grps, main_branches)
-        
-    def merge(self, src_name, commit):
-        branch = self.branches[src_name]
-        try:
-            while True:
-                print branch, branch.svn_path
-                branch = branch.next()
-        except StopIteration:
-            print "trunk reached"
-            
-
-if __name__ == "__main__":
-    tree = Tree()
-    tree.load_tree([("40", []), ("50", []), ("60", [])])
-    print "merging 40"
-    tree.merge("40", None)
+def run():
+    parser = argparse.ArgumentParser(description="merge tool")
+    parser.add_argument("branch", help="the branch to merge")
+    parser.add_argument("commit", help="the commit to merge")
+    args = parser.parse_args()
+    myTree = tree.Tree()
+    myTree.load_tree([("40", []), ("50", []), ("60", [])])
+    myTree.merge(args.branch, args.commit)
     
+if __name__ == "__main__":
+    run()
---
+++ tree.py
@@ -0,0 +1,102 @@
+import pysvn
+import os
+
+DEFAULT_SVN_ROOT = r"https://src.frontsrv.com/svn/repository/branches4/rsk"
+DEFAULT_DISK_ROOT = r"c:\svn"
+
+class Branch(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.disk_root = DEFAULT_DISK_ROOT
+        self.svn_root = DEFAULT_SVN_ROOT
+
+    @property
+    def svn_path(self):
+        return "{}/{}".format(self.svn_root, self.name)
+
+    @property
+    def disk_path(self):
+        return os.path.join(self.disk_root, self.name)
+
+    def next(self):
+        raise NotImplementedError()
+
+    def __repr__(self):
+        return "{}(name={})".format(self.__class__.__name__, self.name)
+
+class ClientBranch(Branch):
+
+    def __init__(self, name, parent):
+        super(ClientBranch, self).__init__(name)
+        self.parent = parent
+
+    def next(self):
+        return self.parent.next_version
+
+class MainBranch(Branch):
+
+    def __init__(self, name, next_version):
+        super(MainBranch, self).__init__(name)
+        self.next_version = next_version
+
+    def next(self):
+        return self.next_version
+
+class Trunk(Branch):
+
+    def __init__(self):
+        super(Trunk, self).__init__("trunk4.1")
+        self.svn_root = r"https://src.frontsrv.com/svn/repository"
+
+    def next(self):
+        raise StopIteration()
+        
+class Tree:
+
+    def __init__(self):
+        self.branches = {"trunk4.1" : Trunk()}
+        self.svn = pysvn.Client()
+
+    def load_main_branches(self, names, head):
+        try:
+            name = names.pop()
+        except IndexError:
+            return []
+        branch = MainBranch(name, head)
+        self.branches[name] = branch
+        return self.load_main_branches(names, branch) + [branch]
+
+    def load_client_branches(self, client_grps, main_branches):
+        for main, clients in zip(main_branches, client_grps):
+            for client in clients:
+                self.branches[client] = ClientBranch(client, main)
+   
+    def load_tree(self, branches):
+        main_branches_names, clients_grps = zip(*branches)
+        main_branches = self.load_main_branches(list(main_branches_names), self.branches["trunk4.1"])
+        self.load_client_branches(clients_grps, main_branches)
+        
+    def merge(self, src_name, commit):
+        branch = self.branches[src_name]
+        try:
+            while True:
+                next_branch = branch.next()
+                self.Client.merge(
+                    branch.disk_path,
+                    commit,
+                    next_branch.disk_path,
+                    commit)
+                print branch, branch.svn_path
+                raise Exception()
+                branch = branch.next()
+        except StopIteration:
+            print "trunk reached"
+            
+
+if __name__ == "__main__":
+    tree = Tree()
+    tree.load_tree([("40", []), ("50", []), ("60", [])])
+    print "merging 40"
+    tree.merge("40", None)
+    
